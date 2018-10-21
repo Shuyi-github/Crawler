9c22156b94688bcae5a7098f4b342238457dfdbd
Tang9511 <31895573+Tang9511@users.noreply.github.com>
2018-10-21 16:18:51 +0800

    1.0

--- code.py
+++ code.py
@@ -1,24 +1,57 @@
 from bs4 import BeautifulSoup
 import requests
 from lxml import html
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
+import re
+import json
+
+def get_city(href,headers,tr='citytr'):
+    base_href="http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2017/"
+    url=base_href+href
+    response= requests.get(url,allow_redirects=False,headers=headers)
+    bsobj=BeautifulSoup(response.content,'lxml')
+    tr_list=bsobj.find_all(class_=tr)
+    city_list=[]
+    for row in tr_list:
+        a_list=row.find_all('a')
+        value=a_list[0].text
+        city=a_list[1].text
+        matchobj=re.search('直辖',city)
+        if matchobj:
+            href=a_list[1].get('href')
+            city_list=city_list+get_city(href,headers,tr='countytr')
+        else:
+            city_list.append({"value":value,"text":city})
+    return city_list
+    
 
 link="http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2017/index.html"
 # link='http://www.baidu.com/'
-s = requests.Session()
+#s = requests.Session()
 headers={
-"User-Agent" :"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
+"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
 "Upgrade-Insecure-Requests": "1"
-        }
-response = s.get(link,allow_redirects=False,headers=headers)
+        } 
+response = requests.get(link,allow_redirects=False,headers=headers)
+#response.enconding ='gb2312'
+#print(response.encoding)
 print(response.status_code)
-print(response.request.headers)
-print(response.text)
-text = BeautifulSoup(response.text,"lxml")
-
-#tree = html.fromstring(page.text)
-#soup = BeautifulSoup(open(link))
+#print(response.request.headers)
+str=response.content.decode('gb2312')
+#print(str)
+bsobj=BeautifulSoup(response.content,'lxml') #将网页源码构造成BeautifulSoup对象，方便操作
+a_list=bsobj.find(class_='provincetable').find_all('a')
+province=[]
+for a in a_list:
+    href=a.get('href')
+    name=a.text
+    id=href.split('.')[0]
+    matchobj=re.search('市',name)
+    if matchobj:
+        href=id+'/'+id+'01.html'
+        city_list=get_city(href,headers,tr='countytr')
+    else:
+        city_list=get_city(href,headers)
+    province.append({"id":id,"name":name,"children":city_list})
+with open('city.json','w') as fp:
+    json.dump(province,fp,ensure_ascii=False) #需要在open时加上encoding=‘utf-8'
+    
